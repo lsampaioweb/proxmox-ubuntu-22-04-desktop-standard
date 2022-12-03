ec0fa337f324b3936d916c103706074e23dca44f
Luciano Sampaio <lsampaioweb@gmail.com>
2022-12-03 08:34:21 -0300

    The build name is a variable now.

--- packer/README.md
+++ packer/README.md
@@ -5,11 +5,6 @@ Run these commands on the computer that is running Packer:
 
 ```bash
   cd packer
-
-  packer init config.pkr.hcl
-  packer build -only="credencials.null.password" -var-file=project.pkrvars.hcl clone/.
-  packer build -only="template.proxmox-clone.ubuntu" -var-file=project.pkrvars.hcl clone/.
-  or 
   ./pipeline.sh
 ```
 
--- packer/pipeline.sh
+++ packer/pipeline.sh
@@ -3,4 +3,4 @@ set -e # Abort if there is an issue with any build.
 
 packer init config.pkr.hcl 
 packer build -only="credencials.null.password" -var-file=project.pkrvars.hcl clone/. 
-packer build -only="template.proxmox-clone.ubuntu" -var-file=project.pkrvars.hcl clone/. 
+packer build -only="ubuntu.proxmox-clone.template" -var-file=project.pkrvars.hcl clone/. 
