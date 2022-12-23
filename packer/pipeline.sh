37c85784f71dd5ae6a8bb7e763f6ee9fe771a744
Luciano Sampaio <lsampaioweb@gmail.com>
2022-12-23 12:24:27 -0300

    Added the validate command.

--- packer/pipeline.sh
+++ packer/pipeline.sh
@@ -2,5 +2,9 @@
 set -e # Abort if there is an issue with any build.
 
 packer init config.pkr.hcl 
+
+# packer validate -only="credencials.null.password" -var-file=project.pkrvars.hcl clone/. 
 packer build -only="credencials.null.password" -var-file=project.pkrvars.hcl clone/. 
+
+# packer validate -only="ubuntu.proxmox-clone.template" -var-file=project.pkrvars.hcl clone/. 
 packer build -only="ubuntu.proxmox-clone.template" -var-file=project.pkrvars.hcl clone/. 
