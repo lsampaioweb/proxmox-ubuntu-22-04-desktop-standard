1f5fbdecb638fd53a97c7bf2ef7f7ae505bb3a45
Luciano Sampaio <lsampaioweb@gmail.com>
2023-03-04 15:59:23 -0300

    Updated the packer bash and created the options for multiple environments.

---
+++ packer/pkr.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e # Abort if there is an issue with any build.
+
+# Usage:
+#   ./pkr.sh validate staging
+#   $1 -> validate, build.
+#   $2 -> staging, production.
+
+# Initialize (download) the packer plugins.
+packer init config.pkr.hcl
+
+# Format all the current and child folders.
+packer fmt -recursive .
+
+packer $1 -only="credencials.null.password" -var-file=$2/vars.pkrvars.hcl clone/. 
+
+packer $1 -only="ubuntu.proxmox-clone.template" -var-file=$2/vars.pkrvars.hcl clone/. 
